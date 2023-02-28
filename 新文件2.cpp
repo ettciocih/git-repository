edc80657f9c651001a083e36eb6b8623c1ea2a4f
ettciocih <3040493597@qq.com>
2023-02-28 08:33:38 +0800

    new

---
+++ 新文件2.cpp
@@ -0,0 +1,80 @@
+任何表达式语句都是表达式加分号组成的。T
+语句printf("%.2f", 1.0 / 3);
+输出为0.33T
+C语言中以 % o 开头的数是八进制整数。F X
+C程序中定义的变量，代表内存中的一个存储单元。T
+字符‘\0’的ASCII码值是0。T
+
+执行以下程序段，输入10，输出10.00。X
+double x;
+scanf("%d", &x);
+printf("%.2f", x);
+F
+
+在C语言中，仅由一个分号（;
+）构成的语句称为空语句，它什么也不做。T
+C语言中，每个字符型数据在ASCII字符集中都有一个惟一的次序值，即ASCII码。T
+
+下列语句中， ____ 将输出 % d B
+A.
+printf(“ % d”);
+B.
+printf(“ % % d”);
+C.
+printf(“ % d”);
+D.
+printf(“ % % % d”);
+
+scanf()函数在读不到数据时返回（）C
+A.
+NULL
+B.
+0
+C.
+EOF
+D.
+1
+
+设a = 5, b = 6, c = 7, d = 8, m = 2, n = 2，执行(m = a > b） && (n = c > d)后n的值为2。T X
+                                  The arithmetic operators, /, %, + and - are in the same level of precedence.X
+
+                                  在 C 程序中，9ab是非法的用户标识符。T
+                                  执行以下初始化后，bi的值是0x5678。
+
+                                  执行以下程序段后，k的值为4。
+
+                                  int k = 3;
+                                  sizeof(++k); F X
+
+                                  表达式(a ^a)的值恒为0。X
+                                  表达式 (3 == x == 3) 的值恒为0。X
+                                  int x; 计算(double)x后，x的类型为double X F
+                                  C语言中的移位运算符（如 << 、 >> ）只能用于整型（如int、short、long、char等），不能用于浮点型（如double、float）。T
+                                  可以使用getchar函数输入一个字符串。F
+                                  运行包含以下代码段的程序将可能进入死循环。
+
+                                  int i = 1；
+
+while (i > 0) {
+i++;
+printf("%d ", i)
+	;
+} F
++ +j表示j先自加, 然后返回j, 此时j = 4, (++j)的值为4
+                                    j++表示返回j后自加, 此时j = 4, (j++)的值为4
+                                    所以表达式的值为8, 表达式算完之后, j = 5T
+
+                                            若有定义：int a＝3, b;
+
+                                    则执行语句：b = (a++，a++，a++); 后，
+
+                                    変量a和b的值分别是（ ）。C
+
+
+                                    A.
+                                    8, 7
+                                    B.
+                                    6, 3
+                                    C.
+                                    6, 5
+                                    D.
